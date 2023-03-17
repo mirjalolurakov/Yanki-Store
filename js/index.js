9a898d56065454e7f1b04a763d0b3a61cceed3b1
Mirjalol <94471421+mirjalolurakov@users.noreply.github.com>
2023-03-17 12:29:07 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     // direction: 'gorizontal',
+//     // loop: false,
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
+
+
 const carousel = document.querySelector(".carousel"),
-firstImg = carousel.querySelectorAll("img")[0],
-arrowIcons = document.querySelectorAll(".wrapper i");
+    firstImg = carousel.querySelectorAll("img")[0],
+    arrowIcons = document.querySelectorAll(".wrapper i");
 
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
@@ -22,14 +45,14 @@ arrowIcons.forEach(icon => {
 
 const autoSlide = () => {
     // if there is no image left to scroll then return from here
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    if (carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
 
     positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
     let firstImgWidth = firstImg.clientWidth + 14;
     // getting difference value that needs to add or reduce from carousel left to take middle img center
     let valDifference = firstImgWidth - positionDiff;
 
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+    if (carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
     // if user is scrolling to the left
@@ -45,7 +68,7 @@ const dragStart = (e) => {
 
 const dragging = (e) => {
     // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
+    if (!isDragStart) return;
     e.preventDefault();
     isDragging = true;
     carousel.classList.add("dragging");
@@ -58,7 +81,7 @@ const dragStop = () => {
     isDragStart = false;
     carousel.classList.remove("dragging");
 
-    if(!isDragging) return;
+    if (!isDragging) return;
     isDragging = false;
     autoSlide();
 }
@@ -71,3 +94,29 @@ carousel.addEventListener("touchmove", dragging);
 
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+let openMenu = document.querySelector(".open_menu");
+let closeMenu = document.querySelector(".close_menu");
+let menu = document.querySelector("#ul");
+let modalContent = document.querySelector(".modal_content");
+let modalBg = document.querySelector(".modal_bg");
+
+openMenu.onclick = () => {
+    menu.style.display = "block";
+    modalContent.style.display = "block";
+    modalBg.style.display = "block";
+
+    setTimeout(() => {
+        modalContent.style.opacity = "1";
+        modalBg.style.opacity = "1";
+    }, 200);
+};
+closeMenu.onclick = () => {
+    modalContent.style.opacity = "0";
+    modalBg.style.opacity = "0";
+    setTimeout(() => {
+        menu.style.display = "none";
+        modalContent.style.display = "none";
+        modalBg.style.display = "none";
+    }, 200);
+};
